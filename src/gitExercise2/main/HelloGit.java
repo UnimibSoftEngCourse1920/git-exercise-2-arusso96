a29f5c8a3f2da964b2ef58ff964c02085021e90c
Alberto Russo <a.russo96@campus.unimib.it>
2019-10-22 10:32:54 +0200

    Aggiunta nuova stampa

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -2,6 +2,8 @@ package gitExercise2.main;
 
 public class HelloGit {
 	public static void main (String [] args) {
-		System.out.println("Hello git!!!");
+		System.out.println("Questa è una stampa");
+		System.out.println("Questa è una stampa");
+		System.out.println("Questa è una stampa");
 	}
 }
